3a2b7011929c31f91d3f1c95be236272a06cd295
Marcos Anthony <130773144+marcosanthony7@users.noreply.github.com>
2024-07-18 12:01:48 -0300

    att index.js e criação do arquivo passaros.js

--- index.js
+++ index.js
@@ -3,12 +3,44 @@ const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(express.json());
+
+app.use('/passaros', passaros);
+
+app.route('/livro')
+  .get((req, res) => {
+    res.send('Retorna um livro aleatório');
+  })
+  .post((req, res) => {
+    res.send('Adiciona um livro');
+  })
+  .put((req, res) => {
+    res.send('Atualiza o livro');
+  });
+
+app.get('/ifalriolargo', (req, res) => {
+  res.send('IFAL RIO LARGO');
 });
 
 app.post('salvar', (req, res) => {
-  res.send('Hello World!');
+  let nome = req.body.nome;
+  let nota = req.body.nota;
+
+  if (nota < 6){
+    req.status(500).json({
+      mensagem: 'Estudar MAIS'
+    });
+    // return;
+  }
+
+  console.log('BORAAA');
+  let pessoa = {
+    nome: nome,
+    turma: '3º ano',
+    instituto: 'IFAL-RL'
+  }
+
+  res.json(pessoa);
 });
 
 app.listen(port, () => {
