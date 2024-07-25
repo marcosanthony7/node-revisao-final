cbe02f72c155e5ee48de5a2925a48288fad8c8b7
Marcos Anthony <130773144+marcosanthony7@users.noreply.github.com>
2024-07-25 11:23:08 -0300

    att exercicio-trycatch.js

--- exercicio-trycatch.js
+++ exercicio-trycatch.js
@@ -1,20 +1,47 @@
+class DivisaoPorZeroError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = 'DivisaoPorZeroError';
+    }
+}
+
+class DivisaoMenorQueUmError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = 'DivisaoMenorQueUmError';
+    }
+}
+
 let carregando = false;
+
 try {
     carregando = true;
     let a = 10;
-    let b = 11;
+    let b = 2;
 
-    if(!b) throw Error('ERRO DE DIVISÃO POR ZERO');
+    if (b === 0) {
+        throw new DivisaoPorZeroError('ERRO DE DIVISÃO POR ZERO!');
+    }
 
     let result = a / b;
 
-    if(result < 1) throw new Error('A DIVISÃO NÃO PODE SER MENOR QUE 1');
-    
+    if (result < 1) {
+        throw new DivisaoMenorQueUmError('A DIVISÃO NÃO PODE SER MENOR QUE 1!');
+    }
+
     console.log('Calculou');
     console.log(result);
 } catch (error) {
-    console.log('ERRO!');
-    console.log(error.message);
+    if (error instanceof DivisaoPorZeroError) {
+        console.log('Erro de divisão por zero!');
+        console.log(error.message);
+    } else if (error instanceof DivisaoMenorQueUmError) {
+        console.log('Erro de divisão menor que 1!');
+        console.log(error.message);
+    } else {
+        console.log('Erro desconhecido:');
+        console.log(error.message);
+    }
 } finally {
     carregando = false;
     console.log('SEMPRE EXECUTADO!');
